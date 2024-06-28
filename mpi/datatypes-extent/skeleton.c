bf305b9ce9b196d84d082ff594b496c6549771e3
Oscar Lostes Cazorla <oscarlostes@gmail.com>
2024-06-28 10:45:34 +0300

    MPI: User-Defined DTypes Extent exercise

--- mpi/datatypes-extent/skeleton.c
+++ mpi/datatypes-extent/skeleton.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     int rank;
-    int array[8][6];
+    int array[8][6], recv_array[8][6];
     MPI_Datatype vector;
 
     MPI_Init(&argc, &argv);
@@ -25,6 +25,12 @@ int main(int argc, char **argv)
         }
     }
 
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 6; j++) {
+            recv_array[i][j] = -1;
+        }
+    }
+
     // Print data on rank 0
     if (rank == 0) {
         printf("Data on rank %d\n", rank);
@@ -37,23 +43,24 @@ int main(int argc, char **argv)
     }
 
     // Create datatype
-    // TODO
+    MPI_Type_vector(8, 1, 6, MPI_INT, &vector);
+    MPI_Type_create_resized(vector, 0, sizeof(int), &vector);
+    MPI_Type_commit(&vector);
 
-    // Send data from rank 0 to rank 1
-    // TODO
+    // Scatter columns
+    MPI_Scatter(array, 1, vector,
+                recv_array, 1, vector, 0, MPI_COMM_WORLD);
 
     // Free datatype
-    // TODO
+    MPI_Type_free(&vector);
 
     // Print received data
-    if (rank == 1) {
-        printf("Received data on rank %d\n", rank);
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 6; j++) {
-                printf("%3d", array[i][j]);
-            }
-            printf("\n");
+    printf("Received data on rank %d\n", rank);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 6; j++) {
+            printf("%3d", recv_array[i][j]);
         }
+        printf("\n");
     }
 
     MPI_Finalize();
