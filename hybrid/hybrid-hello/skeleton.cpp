facd28b9958c0d54ebed0fdf7e83905046dd188f
Oscar Lostes Cazorla <oscarlostes@gmail.com>
2024-06-29 09:51:53 +0300

    Hybrid: Hello World!

--- hybrid/hybrid-hello/skeleton.cpp
+++ hybrid/hybrid-hello/skeleton.cpp
@@ -8,12 +8,28 @@ int main(int argc, char *argv[])
     int provided, required=MPI_THREAD_FUNNELED;
 
     /* TODO: Initialize MPI with thread support. */
+    MPI_Init_thread(&argc, &argv, required, &provided);
 
     /* TODO: Find out the MPI rank and thread ID of each thread and print
      *       out the results. */
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+    
+    #pragma omp parallel private(omp_rank)
+    {
+        omp_rank = omp_get_thread_num();
+        printf("Hello from MPI rank %d, thread %d\n", my_id, omp_rank);
+    }
     /* TODO: Investigate the provided thread support level. */
-
+    if (my_id == 0)
+    {
+        printf("Available thread support levels:\n");
+        printf("\tMPI_THREAD_SINGLE: %d\n", MPI_THREAD_SINGLE);
+        printf("\tMPI_THREAD_FUNNELED: %d\n", MPI_THREAD_FUNNELED);
+        printf("\tMPI_THREAD_SERIALIZED: %d\n", MPI_THREAD_SERIALIZED);
+        printf("\tMPI_THREAD_MULTIPLE: %d\n", MPI_THREAD_MULTIPLE);
+        printf("Thread support level provided: %d\n", provided);
+    }
+    
     MPI_Finalize();
     return 0;
 }
