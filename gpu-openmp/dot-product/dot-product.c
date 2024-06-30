df3662a11e4346d63f06c1a4df62a9a0f59fadea
Oscar Lostes Cazorla <oscarlostes@gmail.com>
2024-06-30 14:31:00 +0300

    OMP-GPU: Dot Product exercise

--- gpu-openmp/dot-product/dot-product.c
+++ gpu-openmp/dot-product/dot-product.c
@@ -15,6 +15,7 @@ int main(void)
     // TODO start: offload and parallelize the computation
 
     double res = 0.0;
+    #pragma omp target teams distribute parallel for map(to: vecA, vecB) reduction(+:res)
     for (int i = 0; i < NX; i++) {
         res += vecA[i] * vecB[i];
     }
